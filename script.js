81df823bc7a3da02fbca6c68f42f7c6ad97479b9
RealD272 <mrithikad272@gmail.com>
2026-06-20 17:14:48 +0600

    Updated portfolio website

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-var tablinks = document.getElementsByClassName("tablinks");
-var tabcontents = document.getElementsByClassName("tab-contents");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const viewCvBtn = document.getElementById("view-cv-btn");
+const cvModal = document.getElementById("cv-modal");
+const closeCvModal = document.getElementById("close-cv-modal");
 
-function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+}
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
+});
+
+if (viewCvBtn && cvModal) {
+  viewCvBtn.addEventListener("click", () => {
+    cvModal.classList.add("open");
+  });
+}
+
+if (closeCvModal && cvModal) {
+  closeCvModal.addEventListener("click", () => {
+    cvModal.classList.remove("open");
+  });
+}
+
+if (cvModal) {
+  cvModal.addEventListener("click", (event) => {
+    if (event.target === cvModal) {
+      cvModal.classList.remove("open");
     }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-} 
+  });
+}
+
